affbcacbf0749e73b15772b1b72544c8fd9dee78
Camila A. Lizarazo M. <lizarazomarescamilaangely@gmail.com>
2025-03-25 16:08:19 -0500

    Actualizacion

--- LAB_02/ACTIVIDADES/Chocolatina.java
+++ LAB_02/ACTIVIDADES/Chocolatina.java
@@ -1,5 +1,22 @@
 package LAB_02.ACTIVIDADES;
 
 public class Chocolatina {
-    
+    private String marca;
+
+    public Chocolatina(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Chocolatina [Marca: " + marca + "]";
+    }
 }
