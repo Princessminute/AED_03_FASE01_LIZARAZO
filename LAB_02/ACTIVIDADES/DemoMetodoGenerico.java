6af6d5fbd36e634cd934f7ef1f7aa2102f9b5ef5
Camila A. Lizarazo M. <lizarazomarescamilaangely@gmail.com>
2025-03-25 16:17:46 -0500

    Actualizacion

--- LAB_02/ACTIVIDADES/DemoMetodoGenerico.java
+++ LAB_02/ACTIVIDADES/DemoMetodoGenerico.java
@@ -1,5 +1,13 @@
 package LAB_02.ACTIVIDADES;
 
-public class DemoMetodoGenerico {
-    
+class DemoMetodoGenerico {
+    static <T extends Comparable<T>> boolean igualArrays (T[] x, T[] y){
+        if (x.length != y.length)
+        return false;
+        for (int i = 0; i < x.length; i++)
+        if(!x[i].equals(y[i]))
+        return false; //arrays diferentes
+        return true; // Contenido de arrays son equivalentes
+        }
+
 }
---
+++ LAB_02/EJERCICIOS/TestGen.java
@@ -0,0 +1,5 @@
+package LAB_02.EJERCICIOS;
+
+public class TestGen {
+    
+}
