1879d71c5e10629fe4ec7fff6a58594c9da37dd3
Camila A. Lizarazo M. <lizarazomarescamilaangely@gmail.com>
2025-04-08 07:46:52 -0500

    Update

--- LAB_04/ACTIVIDADES/BBIterativo.java
+++ LAB_04/ACTIVIDADES/BBIterativo.java
@@ -2,37 +2,44 @@ package LAB_04.ACTIVIDADES;
 
 import java.util.Scanner;
 
-    public class BBIterativo {
-        int binarySearch(int arr[], int x) {
-            int lo = 0, hi = arr.length - 1;
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (arr[mid] == x)
-                    return mid;
-                if (arr[mid] < x)
-                    lo = mid + 1;
-                else
-                    hi = mid - 1;
-            }
-            return -1;
-        }
-    
-        public static void main(String[] args) {
-            BBIterativo ob = new BBIterativo();
-            int arr[] = { 1, 2, 3, 4, 5 };
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingresa el número a buscar: ");
-            int x = scanner.nextInt();
-            int position = ob.binarySearch(arr, x);
-            if (position == -1)
-                System.out.println("Elemento no encontrado");
+public class BBIterativo {
+    int binarySearch(int arr[], int x) {
+        int lo = 0, hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] < x)
+                lo = mid + 1;
             else
-                System.out.println("Elemento encontrado en el índice: " + position);
-            System.out.print("Arreglo: ");
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
+                hi = mid - 1;
+        }
+        return -1;
     }
+
+    public static void main(String[] args) {
+        BBIterativo ob = new BBIterativo();
+        int arr[] = { 1, 3, 5, 4, 5 };
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa el número a buscar: ");
+        int x = scanner.nextInt();
+
+        long startTime = System.nanoTime();
+        int position = ob.binarySearch(arr, x);
+        long endTime = System.nanoTime();
+
+        if (position == -1)
+            System.out.println("Elemento no encontrado");
+        else
+            System.out.println("Elemento encontrado en el índice: " + position);
+
+        System.out.print("El arreglo es el siguiente: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.println("Tiempo de ejecución (iterativo): " + (endTime - startTime) + " nanosegundos");
     }
+}
+
     
--- LAB_04/ACTIVIDADES/BusquedaBinaria.java
+++ LAB_04/ACTIVIDADES/BusquedaBinaria.java
@@ -3,7 +3,6 @@ package LAB_04.ACTIVIDADES;
 import java.util.Scanner;
 
 public class BusquedaBinaria {
-    
     int binarySearch(int arr[], int lo, int hi, int x) {
         if (hi >= lo && lo < arr.length) {
             int mid = lo + (hi - lo) / 2;
@@ -18,22 +17,28 @@ public class BusquedaBinaria {
 
     public static void main(String[] args) {
         BusquedaBinaria ob = new BusquedaBinaria();
-        int arr[] = { 1, 2, 3, 4, 5 };
+        int arr[] = { 1, 3, 5, 4, 5 };
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresa el número a buscar: ");
         int x = scanner.nextInt();
-        int n = arr.length;
-        int position = ob.binarySearch(arr, 0, n - 1, x);
+
+        long startTime = System.nanoTime();
+        int position = ob.binarySearch(arr, 0, arr.length - 1, x);
+        long endTime = System.nanoTime();
+
         if (position == -1)
             System.out.println("Elemento no encontrado !!!");
         else
             System.out.println("Elemento encontrado en el índice: " + position);
+
         System.out.print("El arreglo es el siguiente: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
         System.out.println();
+        System.out.println("Tiempo de ejecución (recursivo): " + (endTime - startTime) + " nanosegundos");
     }
 }
+
 
     
